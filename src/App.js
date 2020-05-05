f69914e6d8023b8fda0f955340c185129049b9c3
DEV <connortodd0304@gmail.com>
2020-05-05 15:53:52 +0100

    livecode with comments implemented

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // We are retrieving the local storage item if it exists
+      // It needs to be parsed first as it is stored as a string
+      // If the local storage item does not exist we set a default value to 0
+      counter: JSON.parse(localStorage.getItem('counter')) || 0
+    }
+  }
+
+  // This method simply handles increments or decrements based off of the 
+  // operator received within the parameter
+  handleCounter = (operator) => {
+    let counter = this.state.counter
+    if (operator === '+') {
+      counter += 1
+    } else {
+      counter -= 1
+    }
+
+    this.setState({ counter })
+
+    // We set the local storage item with the first string being
+    // the name this is always a string.
+    // The second value we need to stringify the item in order
+    // for it to be stored correctly on the local storage
+    localStorage.setItem('counter', JSON.stringify(counter))
+  }
+
+  render() {
+    const { counter } = this.state;
+    return (
+      <div className="App">
+        <h1>{counter}</h1>
+        <button onClick={() => this.handleCounter('+')}>+</button>
+        <button onClick={() => this.handleCounter('-')}>-</button>
+      </div>
+    );
+  }
 }
 
 export default App;
